543e382c82f17bdd4e6387723c81ad4ff0459a9f
Nick Johnstone <ncwjohnstone@gmail.com>
2015-11-17 21:53:19 +1300

    Add failing test

--- test/animation-driver-test.js
+++ test/animation-driver-test.js
@@ -1,7 +1,19 @@
 /* globals describe, it */
 
+import Rx from 'rx';
+import makeAnimationDriver from '../src/driver';
+
+import assert from 'assert';
+
 describe('Animation driver', () => {
-  it('provides an observable of requestAnimationFrame events', () => {
+  it('provides an observable of requestAnimationFrame events', (done) => {
+    const driver = makeAnimationDriver();
+
+    const responses = driver(Rx.Observable.empty());
+
+    responses.subscribe(val => assert.equal(typeof val, 'number'));
+
+    done();
   });
 });
 
