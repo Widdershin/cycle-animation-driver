c02038a35060c3e8f62402c23dfb322a2ea08c7b
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-07 21:04:57 +1300

    It's working!
    https://www.youtube.com/watch?v=AXwGVXD7qEQ

--- src/driver.js
+++ src/driver.js
@@ -1,2 +1,16 @@
-export default function () {
+import Rx from 'rx';
+
+export default function makeAnimationDriver () {
+  return function animationDriver () {
+    const animation$ = new Rx.Subject();
+
+    function tick (timestamp) {
+      animation$.onNext(timestamp);
+      requestAnimationFrame(tick);
+    }
+
+    tick(new Date().valueOf());
+
+    return animation$;
+  };
 }
--- test/animation-driver-test.js
+++ test/animation-driver-test.js
@@ -3,7 +3,11 @@
 import Rx from 'rx';
 import makeAnimationDriver from '../src/driver';
 
+import raf from 'raf';
+
 import assert from 'assert';
+
+global.requestAnimationFrame = raf;
 
 describe('Animation driver', () => {
   it('provides an observable of requestAnimationFrame events', (done) => {
@@ -11,9 +15,28 @@ describe('Animation driver', () => {
 
     const responses = driver(Rx.Observable.empty());
 
-    responses.subscribe(val => assert.equal(typeof val, 'number'));
+    let firstTimeValue;
 
-    done();
+    let index = 0;
+
+    responses.take(2).subscribe(val => {
+      if (index === 0) {
+        firstTimeValue = val;
+
+        assert.equal(typeof val, 'number');
+      }
+
+      if (index === 1) {
+        assert(
+          val > firstTimeValue,
+          `Expected ${val} to be > ${firstTimeValue}`
+        );
+
+        done();
+      }
+
+      index += 1;
+    });
   });
 });
 
