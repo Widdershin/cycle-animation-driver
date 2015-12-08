887f0c4223b213469834dcdd26214bb186c8ab8f
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-08 20:19:08 +1300

    Update example!

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ import _ from 'lodash';
 
 const maxSpeed = 500;
 const maxHeight = 100;
+const maxWidth = 15 * 50;
+const maxNodeCount = 100;
 
-function nodes (timestamp, speed, height) {
+function fancyColor (timestamp, i, offset) {
+  return Math.abs(Math.round((timestamp + (offset * 100) + (i * 20)) % 512) - 255);
+}
+
+function nodes (timestamp, speed, height, nodeCount) {
+  const increment = maxWidth / nodeCount;
+
   return (
-    div('.nodes', _.range(1, 50).map(i =>
+    div('.nodes', _.range(1, nodeCount).map(i =>
       div('.node', {
+        key: i,
         style: {
-          left: (i * 15) + 'px',
-          top: (Math.sin(i + timestamp / (maxSpeed - speed)) * height + 150).toString() + 'px'
+          color: `rgb(${fancyColor(timestamp, i, 0)}, ${fancyColor(timestamp, i, 1)}, ${fancyColor(timestamp, i, 2)})`,
+          left: (increment * i) + 'px',
+          top: (Math.sin((increment * i) + timestamp / (maxSpeed - speed)) * height + 150).toString() + 'px'
         }
       }, '.')
     ))
@@ -32,14 +42,22 @@ function main ({DOM, animation}) {
     .map(ev => ev.target.value)
     .startWith(maxHeight / 2);
 
+  const nodeCount$ = DOM
+    .select('.node-count')
+    .events('input')
+    .map(ev => ev.target.value)
+    .startWith(45);
+
   return {
-    DOM: animation.withLatestFrom(speed$, height$, (timestamp, speed, height) =>
+    DOM: animation.pluck('timestamp').withLatestFrom(speed$, height$, nodeCount$, (timestamp, speed, height, nodeCount) =>
       div('.time', [
-        nodes(timestamp, speed, height),
+        nodes(timestamp, speed, height, nodeCount),
         'Speed',
         input('.speed', {type: 'range', min: 1, max: maxSpeed, value: speed}),
         'Height',
-        input('.height', {type: 'range', min: 1, max: maxHeight, value: height})
+        input('.height', {type: 'range', min: 1, max: maxHeight, value: height}),
+        'Nodes',
+        input('.node-count', {type: 'range', min: 1, max: maxNodeCount, value: nodeCount}),
       ])
     )
   };
